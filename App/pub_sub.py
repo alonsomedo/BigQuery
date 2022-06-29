3580ba6157f02003978be5b36abaadf0e1f8724d
Alonso MD <alonso2295@hotmail.com>
2022-06-29 15:16:30 -0500

    creating code to emulate pubsub

---
+++ App/pub_sub.py
@@ -0,0 +1,19 @@
+from google.cloud import pubsub_v1
+import time
+
+
+publisher = pubsub_v1.PublisherClient()
+
+topic_name = 'projects/bigquery-demo-285417/topics/data_stream_from_file' # [project]/[project-name]/[topics]/[topic-name]
+
+try:
+    publisher.create_topic(topic_name)
+except:
+    print('Topic already exists')
+
+with open('food_orders.csv') as file:
+    for row in file:
+        data = row
+        future = publisher.publish(topic_name, data=data)
+        print(future.result())
+        time.sleep(1)

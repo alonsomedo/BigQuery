e08df9274ff859b7450c500b9f0618cd64f1340e
Alonso MD <alonso2295@hotmail.com>
2022-06-29 15:52:05 -0500

    fixing pubsub code

--- App/pub_sub.py
+++ App/pub_sub.py
@@ -4,16 +4,22 @@ import time
 
 publisher = pubsub_v1.PublisherClient()
 
-topic_name = 'projects/bigquery-demo-354214/topics/data_stream_from_file' # [project]/[project-name]/[topics]/[topic-name]
+project_id = 'bigquery-demo-354214'
+topic_id = 'data_stream_from_file' 
+
+topic_path = publisher.topic_path(project_id, topic_id) # [project]/[project-name]/[topics]/[topic-name]
 
 try:
-    publisher.create_topic(topic_name)
+    topic = publisher.create_topic(request={"name": topic_path})
+    print(f"Created topic: {topic.name}")
 except:
     print('Topic already exists')
 
+
+
 with open('food_orders.csv') as file:
     for row in file:
-        data = row
-        future = publisher.publish(topic_name, data=data.encode("utf-8"))
+        data = row.encode("utf-8")
+        future = publisher.publish(topic_path, data=data)
         print(future.result())
         time.sleep(1)
